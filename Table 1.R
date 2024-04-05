0b6a8717d438ad1712ee6e3caef2ddaee99e54d3
Nisha Narayanan <89948867+Narayanan-Nisha@users.noreply.github.com>
2024-04-04 22:43:10 -0500

    Update Table 1.R

--- Table 1.R
+++ Table 1.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(gtsummary)
-rppa_data <- read.table("/Users/nnarayanan/Library/CloudStorage/OneDrive-InsideMDAnderson/ASNS/RPPA_Data.csv", sep = "\t")
+rppa_data <- read.table("RPPA_Data.csv", sep = "\t")
 rppa_data$FAB <- rppa_data$PseudoFAB
 rppa_data$FAB <- gsub("M4e", "M4", rppa_data$FAB)
 rppa_data$FAB <- gsub("M5a", "M5", rppa_data$FAB)
@@ -29,5 +29,5 @@ table0 <- tbl_summary(clin_data,
 modify_0 <- table0 %>% modify_caption("**Demographic and clinical characteristics of AML patients in the study set** (N = {N})")
 t0 <- add_q(modify_0, method = "fdr", pvalue_fun = NULL, quiet = NULL) %>% as_gt()
 #gt::gtsave(t0, filename = "Clinical_Table_by_Medians.png")
-gt::gtsave(t0, filename = "/Users/nnarayanan/Library/CloudStorage/OneDrive-InsideMDAnderson/NishaNarayanan/ASNS/Code/Table 1/Demographic_Clinical_Table.docx") 
+gt::gtsave(t0, filename = "Demographic_Clinical_Table.docx") 
 
