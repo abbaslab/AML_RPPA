5ec64733f9de3c8d37089ea6155303f0c86330e6
Nisha Narayanan <89948867+Narayanan-Nisha@users.noreply.github.com>
2024-04-04 22:36:49 -0500

    Update Fig1_ASNS_expression.R

--- Fig1_ASNS_expression.R
+++ Fig1_ASNS_expression.R
@@ -1,7 +1,7 @@
 library(ggplot2)
-rppa_data <- read.table("/Users/nnarayanan/Library/CloudStorage/OneDrive-InsideMDAnderson/ASNS/RPPA_Data.csv", sep = "\t")
+rppa_data <- read.table("/RPPA_Data.csv", sep = "\t")
 
-pdf("/Users/nnarayanan/Library/CloudStorage/OneDrive-InsideMDAnderson/NishaNarayanan/ASNS/Figures/Figure 1b.pdf")
+pdf("Figure 1b.pdf")
 ggplot(rppa_data, aes(x = ASNS)) + 
   geom_histogram(aes(y = after_stat(density)), binwidth = 0.5, color = "black",fill = "steelblue3") + 
   geom_density(color="red", lwd=1)  +
@@ -9,17 +9,4 @@ ggplot(rppa_data, aes(x = ASNS)) +
   ylab("Density") +
   theme_classic()
 dev.off()
-mean(rppa_data$ASNS)
-median(rppa_data$ASNS)
-shapiro.test(rppa_data$ASNS)
-pdf("/Users/nnarayanan/Library/CloudStorage/OneDrive-InsideMDAnderson/NishaNarayanan/ASNS/Figures/Figure S1.pdf")
-qqnorm(rppa_data$ASNS)
-dev.off()
 
-rppa_data$poi_median <- factor(rppa_data$poi_median, levels = c("LowASNS", "HighASNS"))
-rppa_data$OS.Wks.SMK.yr <- rppa_data$OS.Wks.SMK/52
-rppa_data$status <- ifelse(rppa_data$vital.status=="D", 0, 1)
-library(survival)
-library(survminer)
-fit <- survfit(Surv(OS.Wks.SMK, status==0) ~ 1, data = rppa_data)
-surv_median(fit)
